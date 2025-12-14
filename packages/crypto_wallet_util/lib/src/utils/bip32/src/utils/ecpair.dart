f32be71a69f72b5151eb04ad44291e7d00ad30cf
yami <yami-rnd@skvip.org>
2025-12-14 13:49:37 +0800

    Fix: add testnet definition

--- packages/crypto_wallet_util/lib/src/utils/bip32/src/utils/ecpair.dart
+++ packages/crypto_wallet_util/lib/src/utils/bip32/src/utils/ecpair.dart
@@ -46,6 +46,15 @@ final bitcoin = NetworkType(
   wif: 0x80,
 );
 
+final testnet = NetworkType(
+  messagePrefix: '\x18Bitcoin Signed Message:\n',
+  bech32: 'tb',
+  bip32: Bip32Type(public: 0x043587cf, private: 0x04358394),
+  pubKeyHash: 0x6f,
+  scriptHash: 0xc4,
+  wif: 0xef,
+);
+
 class ECPair {
   Uint8List? _d;
   Uint8List? _Q;
